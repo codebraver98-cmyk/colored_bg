aaf015835aff2b4c9e221545bfd81673227c882d
codebraver98-cmyk <codebraver98@codebrave.org>
2026-09-24 15:40:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("click",function() { 
+    document.getElementById('red').style.backgroundColor='red';
+})
+window.addEventListener("click",function() {
+    document.getElementById('blue').style.backgroundColor='blue';
+})
+window.addEventListener("click",function() {
+    document.getElementById('green').style.backgroundColor='green';
+})
+
+window.addEventListener("click",function() {
+    document.getElementById('red').style.backgroundColor='whitesmoke';
+    document.getElementById('green').style.backgroundColor='whitesmoke';
+    document.getElementById('blue').style.backgroundColor='whitesmoke';
+})
